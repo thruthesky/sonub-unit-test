112f2ad58bf9985fde87bde05c756cc2818ca78e
JaeHo Song <thruthesky@gmail.com>
2017-09-13 22:36:00 +0800

    update

--- src/nightmare.ts
+++ src/nightmare.ts
@@ -1,13 +1,133 @@
 let Nightmare = require('nightmare');
+const c = require('cheerio');
+import { config } from './confis';
+
+
+
+
 
 
 let defaultOptions = {
-    show: true, x: 1024, y:0, width: 640,
-    openDevTools: { mode: ''}
+    show: true, x: 1024, y: 0, width: 640,
+    openDevTools: { mode: '' }
 };
+
+
+
 export function getNightmare() {
-    let nightmare = Nightmare( defaultOptions );
+    return get();
+}
+
+
+
+
+
+export function get() {
+    let nightmare = Nightmare(defaultOptions);
     return nightmare;
 }
 
 
+
+export class Mare {
+    nightmare;
+    constructor() {
+        this.nightmare = get();
+    }
+
+
+    get urlHome() {
+        return config.url;
+    }
+
+    async html(url = null): Promise<any> {
+        if (url) {
+            await this.nightmare.goto(config.url);
+        }
+        let html = await this.nightmare
+            .evaluate(() => document.querySelector('html').innerHTML)
+            .then(html => html);
+        let $html = c.load(html)('html');
+        return $html;
+    }
+    async submit( selector = null ) {
+        if ( selector ) return this.nightmare.click(selector);
+        return await this.nightmare.click('.page-form-submit');
+    }
+
+    test(re, msg = '') {
+        if (re === true) this.success(msg);
+        else this.failure(msg);
+    }
+
+    success(msg) {
+        console.log("SUCCESS: ", msg);
+    }
+    failure(msg) {
+        console.log("FAILURE: ", msg);
+    }
+
+
+
+    click(selector) {
+        return this.nightmare.click(selector);
+    }
+    wait(selector) {
+        return this.nightmare.wait(selector);
+    }
+    type(selector, text) {
+        return this.nightmare.type(selector, text);
+    }
+    enter(selector) {
+        return this.nightmare.type(selector, "\x0d");
+    }
+    async typeEnter(selector, text) {
+        await this.type(selector, text);
+        await this.enter(selector);
+    }
+
+
+    async waitTest( wait, test, msg ) {
+        await this.wait(wait);
+        let $h = await this.html();
+        await this.test($h.find(test).length > 0, msg);
+    }
+    async clickWaitTest(click, wait, test, msg) {
+        await this.click(click);
+        await this.waitTest( wait, test, msg );
+    }
+
+    async submitWaitTest(submit, wait, test, msg) {
+        await this.submit(submit);
+        // await this.wait(wait);
+        // await this.testSelector(test, msg);
+        await this.waitTest( wait, test, msg );
+    }
+    async testSelector(selector, msg) {
+        let $h = await this.html();
+        // console.log("title: ", $h.find('title').text());
+        // console.log("count: ", $h.find(selector).length);
+        this.test($h.find(selector).length > 0, msg);
+    }
+
+
+    async closeAlert() {
+        await this.click('button.alert-close').wait(100).then();
+    }
+
+    async openMenuPage() {
+        await this.clickWaitTest('#header-menu-icon', '#menu-page-header', '#menu-page-header', 'Open menu page. Check menu page header' ); // menu page
+    }
+
+    async openCommunityPage() {
+        await this.openMenuPage();
+        await this.clickWaitTest('#menu-community', '#community-header', '#community-header', "Open community page. Checking header.");
+    }
+    async openAskPage() {
+        await this.openCommunityPage();
+        await this.clickWaitTest('#community-qna-button', '#post-list-qna', '#post-list-qna', "Open post list page. Qna");
+    }
+
+
+
+}
--- src/tests/open.ts
+++ src/tests/open.ts
@@ -5,7 +5,7 @@
  * 
  */
 import { config } from './../confis';
-import { getNightmare } from '../nightmare';
+import { getNightmare, Mare } from '../nightmare';
 import * as t from '../test';
 const c = require('cheerio');
 
@@ -17,64 +17,146 @@ run();
 
 async function run() {
 
-    let html = await n.goto(url)
-        .evaluate(() => document.querySelector('html').innerHTML)
-        .then(html => html);
-
-    let $html = c.load(html)('html');
-
-    console.log("count: ", $html.find('.page').length);
-
-    t.isTrue($html.find('title').length == 1, "Site open..");
+    let $n = new Mare();
+    let $h = await $n.html($n.urlHome);
+    $n.test($h.find('title').length == 1, "Site open..");
+    $n.test($h.find('#header-menu-icon').length == 1, 'check menu icon');
 
 
-
-    t.isTrue($html.find('#header-menu-icon').length == 1, 'check menu icon');
-
-    html = await n.
-        click('#header-menu-icon')
-        .wait('#menu-page-header')
-        .evaluate( () => document.querySelector('html').innerHTML)
-        .then( a => a );
+    await $n.openMenuPage();
+    await $n.clickWaitTest('#menu-page-login', '.login-page', '.login-page', 'check login page.' );
     
-    $html = c.load(html)('html');
-    t.isTrue($html.find('#menu-page-header').length == 1, 'check menu page header..');
+    await $n.submit();
+    await $n.wait('.modal.error');
+    await $n.testSelector('.error-42051', 'Empty email');
 
 
-    html = await n
-        .click('#menu-page-login')
-        .wait('.login-page')
-        .evaluate( () => document.querySelector('html').innerHTML)
-        .then( a => a );
-    $html = c.load(html)('html');
-    t.isTrue( $html.find('.login-page').length == 1, 'Login page check...');
-
-    await checkIf('.error-42051', 'Empty email');
-
-    await closeAlert();
-    await n.type('#register_user_login', 'abcde');
-    await checkIf('.error-42052', 'Empty password');
-   
-    await closeAlert();
-    await n.type('#register_user_pass', 'xxxxxxx');
-    await checkIf('.error-42053', 'No user by that email.');
+    await $n.closeAlert();
+    await $n.type('#register_user_login', 'abcde');
+    await $n.submitWaitTest( '', '.modal.error', '.error-42052', 'Empty password');
 
 
-    await closeAlert();
-    await n.click('#login-register');
+    await $n.closeAlert();
+    await $n.type('#register_user_pass', 'xxxxxxx');
+    await $n.submitWaitTest( '', '.modal.error', '.error-42053', 'No user by that email.');
+
+
+    await $n.closeAlert();
+    await $n.clickWaitTest('#login-register', '#register-submit', '#register-submit', 'Open Register Page.');
+
+    await $n.clickWaitTest('#register-submit', '.error', '.error-8071', 'Register with empty email. Email is required.');
+    await $n.closeAlert();
+    await $n.type('#register-email', 'admin');
+    await $n.clickWaitTest('#register-submit', '.error', '.error-8072', 'Register with empty password. Password is required.');
+    await $n.closeAlert();
+    await $n.type('#register-password', 'o');
+    await $n.submitWaitTest( '', '.modal.error', '.error-40001', 'Email is in use');
+
+    await $n.closeAlert();
+
+
+
+    // ask list
+    // await $n.clickWaitTest('#header-menu-icon', '#menu-page-header', '#menu-page-header', 'check menu page header' ); // menu page
+    await $n.openCommunityPage();
+    await $n.openAskPage();
+
+    // ask write
+    await $n.click('#post-list-create-button');
+    await $n.submit('.post-create-button');
+    await $n.submitWaitTest('.post-create-button', '.error', '.error-42052', 'Anonymous post create without password.');
+    await $n.closeAlert();
+    await $n.click('.post-cancel-button');
+
+
+    // first post of ask forum
+    $h = await $n.html();
+    let $p = $h.find('.posts .post:first-child');
+    // console.log( $p.find('h2').text());
+
+
+    await $n.typeEnter('.posts .post:first-child .comment-content', 'hello');
+    await $n.waitTest('.error', '.error-80005', 'Creating comment without login');
+    await $n.closeAlert();
+
+    // test
+    /// like
+    /// dislike
+    /// reply
+    
+
+
+
+    
+    
+
+
+
+
+
+
+    
+
+
+
+    // let html = await n.goto(url)
+    //     .evaluate(() => document.querySelector('html').innerHTML)
+    //     .then(html => html);
+
+    // let $html = c.load(html)('html');
+
+    // console.log("count: ", $html.find('.page').length);
+
+    // t.isTrue($html.find('title').length == 1, "Site open..");
+
+
+
+    // t.isTrue($html.find('#header-menu-icon').length == 1, 'check menu icon');
+
+    // html = await n.
+    //     click('#header-menu-icon')
+    //     .wait('#menu-page-header')
+    //     .evaluate(() => document.querySelector('html').innerHTML)
+    //     .then(a => a);
+
+    // $html = c.load(html)('html');
+    // t.isTrue($html.find('#menu-page-header').length == 1, 'check menu page header..');
+
+
+    // html = await n
+    //     .click('#menu-page-login')
+    //     .wait('.login-page')
+    //     .evaluate(() => document.querySelector('html').innerHTML)
+    //     .then(a => a);
+    // $html = c.load(html)('html');
+    // t.isTrue($html.find('.login-page').length == 1, 'Login page check...');
+
+
+    // await checkIf('.error-42051', 'Empty email');
+
+    // await closeAlert();
+    // await n.type('#register_user_login', 'abcde');
+    // await checkIf('.error-42052', 'Empty password');
+
+    // await closeAlert();
+    // await n.type('#register_user_pass', 'xxxxxxx');
+    // await checkIf('.error-42053', 'No user by that email.');
+
+
+    // await closeAlert();
+    // await n.click('#login-register');
 }
 
 
-async function closeAlert() {
-    await n.click('button.alert-close').then();    
-}
+// async function closeAlert() {
+//     await n.click('button.alert-close').then();
+// }
 
 
-async function checkIf(selector, msg) {
-    await n.click('#login-submit');
-    await n.wait('.alert-content-bottom');
-    let doc = await n.evaluate( () => document.querySelector('html').innerHTML).then( a => a);
-    let $html = c.load(doc)('html');
-    t.isTrue( $html.find(selector).length == 1, msg);
-
-}
+// async function checkIf(selector, msg) {
+//     await n.click('#login-submit');
+//     await n.wait('.alert-content-bottom');
+//     let doc = await n.evaluate(() => document.querySelector('html').innerHTML).then(a => a);
+//     let $html = c.load(doc)('html');
+//     t.isTrue($html.find(selector).length == 1, msg);
+// }
